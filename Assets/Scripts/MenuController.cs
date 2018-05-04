af4ac4c816871b7802b879b6ecc91b3f71571e83
unknown <coderingio@gmail.com>
2018-05-03 19:58:18 -0600

    Create Main menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+	public Animator panel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartGame(){
+		SceneManager.LoadScene ("UDT");
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+
+	public void RaisePanel(){
+		panel.SetBool("panelUp", true);
+	}
+
+	public void LowerPanel(){
+		panel.SetBool("panelUp", false);
+	}
+}
